0b79fa9e85942ecfae009d3dde5b87235c3d8473
ikubaku <hide4d51@gmail.com>
2021-05-13 17:48:19 +0900

    Implement data acquision

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { ConfigService } from '@nestjs/config';
+
+import Obniz from 'obniz';
+
+import { Measure } from './measure.interface';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+  private latestMeasure: Measure = { weight: 0, datetime: 0 };
+  private obnizId: string;
+
+  constructor(private configService: ConfigService) {
+    this.obnizId = configService.get('OBNIZ_ID');
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron('0 * * * * *')
+  async measure() {
+    this.logger.log('Begin measuring...');
+    const obniz = new Obniz(this.obnizId, {
+      auto_connect: false,
+      reset_obniz_on_ws_disconnection: false,
+    });
+
+    const isConnected = await obniz.connectWait({ timeout: 10 });
+    if (isConnected) {
+      const sensor = obniz.wired('hx711', {
+        gnd: 0,
+        dout: 1,
+        sck: 2,
+        vcc: 3,
+      });
+      sensor.powerUp();
+      const value = await sensor.getValueWait(10);
+      this.logger.log('grams: ' + value);
+      this.latestMeasure.weight = value;
+      this.latestMeasure.datetime = Date.now();
+      sensor.powerDown();
+      await obniz.closeWait();
+    }
+  }
+
+  getWeight(): number {
+    // stub
+    return 42;
   }
 }
